6b66766725e9092f5ad1ea3363363c085edfcc1a
Edd Sowden <e@e26.co.uk>
2015-01-05 11:29:32 +0000

    Refactor tabs into prototypical object
    Also added hashchange support so the back button works.

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,63 +1,107 @@
 (function () {
   "use strict"
 
-  var tabs = {
-    linkClick: function(e) {
-      var $link = $(this),
-          $tabList = $link.closest('.js-tabs'),
-          $tabLinks = $tabList.find('a');
+  function Tabs($tabList, options){
+    // only add tabs to browsers which support onhashchange (IE 8+).
+    if(!"onhashchange" in window) { return; }
 
-      $tabLinks.removeClass('active');
+    if(!options) { options = {} }
+    this.paneIDPrefix = options.panelIDPrefix || 'tab-pane-';
+    this.linkIDPrefix = options.linkIDPrefix || 'tab-link-';
+
+    this.$tabList = $tabList;
+    this.$tabLinks = this.$tabList.find('a');
+
+    // add link listeners
+    this.$tabList.on('click', 'a', this.linkClick.bind(this));
+
+    // add keyboard listeners
+    this.$tabList.on('keydown', 'a', this.arrowNavigate.bind(this));
+
+    // add hash change listener
+    $(window).on('hashchange', this.hashNavigate.bind(this));
+
+    // add aria goodness
+    this.addAriaAttributes();
+
+    // init the tabs state
+    this.updateTabs();
+
+    // navigate to a tab if one is defined
+    this.hashNavigate();
+
+    this.$initalActiveLink = this.$tabLinks.filter('.active');
+  }
+  Tabs.prototype = {
+    linkClick: function(e) {
+      var $link = $(e.target);
+
+      this.$tabLinks.removeClass('active');
       $link.addClass('active');
 
-      tabs.updateTabs($tabLinks);
+      this.updateTabs();
+
+      window.location.hash = $link.attr('href');
 
       e.preventDefault()
     },
     arrowNavigate: function(e) {
-      var $link = $(this),
-          $tabList = $link.closest('.js-tabs'),
-          $tabLinks = $tabList.find('a'),
-          currentIndex = $tabLinks.index($link);
+      // return if it looks like we are trying to do something else
+      if(e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) { return; }
+
+      var $link = $(e.target),
+          currentIndex = this.$tabLinks.index($link);
 
       if(e.which === 37 || e.which === 38){ // left or up
         if(currentIndex > 0) {
-          $tabLinks.eq( currentIndex - 1 ).focus().click();
+          this.$tabLinks.eq( currentIndex - 1 ).focus().click();
         } else {
-          $tabLinks.eq( $tabLinks.length - 1 ).focus().click();
+          this.$tabLinks.eq( this.$tabLinks.length - 1 ).focus().click();
         }
       } else if (e.which === 39 || e.which === 40){ // right or down
-        if(currentIndex < $tabLinks.length - 1) {
-          $tabLinks.eq( currentIndex + 1 ).focus().click();
+        if(currentIndex < this.$tabLinks.length - 1) {
+          this.$tabLinks.eq( currentIndex + 1 ).focus().click();
         } else {
-          $tabLinks.eq( 0 ).focus().click();
+          this.$tabLinks.eq( 0 ).focus().click();
         }
       }
     },
-    addAriaAttributes: function($tabList, $linkTabs) {
-      $tabList.attr('role', 'tablist');
-      $linkTabs.each(function(i, el){
+    hashNavigate: function(event){
+      if(window.location.hash !== ''){
+        this.$tabLinks.filter('a[href="' + window.location.hash +'"]').focus().click();
+      } else if(window.location.hash === '' && event){
+        this.$initalActiveLink.focus().click();
+      }
+    },
+    addAriaAttributes: function() {
+      this.$tabList.attr('role', 'tablist');
+      this.$tabLinks.each(function(i, el){
         var $tabLink = $(el),
             tabId = $tabLink.attr('href').substr(1),
-            linkId = 'tab-link-' + tabId,
+            $tab = $('#' + tabId),
+            linkId = this.linkIDPrefix + tabId,
+            newTabId = this.paneIDPrefix + tabId,
             $tab = $('#'+tabId);
+
+        $tab.attr('id' , newTabId);
 
         $tabLink.attr({
           'id': linkId,
           'role': 'tab',
-          'aria-controls': tabId
+          'aria-controls': newTabId
         });
         $tab.attr({
           'aria-labelledby': linkId,
           'role': 'tabpanel',
           'tabindex': '-1'
         });
-      });
+      }.bind(this));
     },
-    updateTabs: function($tabLinks){
-      $tabLinks.each(function(i, el){
+    updateTabs: function(){
+      this.$tabLinks.each(function(i, el){
         var $tabLink = $(el),
-            $tab = $($tabLink.attr('href')),
+            tabId = this.paneIDPrefix + $tabLink.attr('href').substr(1),
+            $tab = $('#' + tabId),
             active = $tabLink.hasClass('active');
 
         $tabLink.attr({
@@ -69,28 +113,9 @@
           'aria-expanded': (active ? 'true' : 'false')
         });
         $tab.css('display', (active ? '' : 'none'));
-      });
-    },
-    init: function(){
-      // find all the tab lists and iterate over them
-      $('.js-tabs').each(function(i, el){
-        var $tabList = $(el),
-            $tabLinks = $tabList.find('a');
-
-        // add link listeners
-        $tabLinks.click(tabs.linkClick);
-
-        // add keyboard listeners
-        $tabLinks.keydown(tabs.arrowNavigate);
-
-        // add aria goodness
-        tabs.addAriaAttributes($tabList, $tabLinks);
-
-        // init the tabs state
-        tabs.updateTabs($tabLinks);
-      });
+      }.bind(this));
     }
   }
 
-  window.tabs = tabs;
+  window.Tabs = Tabs;
 }());
